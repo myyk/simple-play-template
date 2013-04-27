5fa227cc93c19c98ebb17c611d8cf9448b42137e
Myyk Seok <myyk.seok@gmail.com>
2013-04-26 22:03:03 -0700

    Add WebJars.

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,20 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "simple-play-template-1"
+  val appName         = "simple-play-template"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "com.github.twitter" % "bootstrap" % "2.0.2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    resolvers += "webjars" at "http://webjars.github.com/m2"
   )
 
 }
